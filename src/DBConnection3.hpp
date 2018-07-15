efd7cfb7913b86f12b19caae1982e09754d152fc
Konstantin Ziabrev <konstantin.ziabrev@gmail.com>
2018-07-15 08:36:41 +0300

    Adding clang-format check

--- src/DBConnection3.hpp
+++ src/DBConnection3.hpp
@@ -5,31 +5,32 @@
 #include "DBConnection.hpp"
 
 class IDBConnection3 {
-public:
-    virtual ~IDBConnection3() = default;
+ public:
+  virtual ~IDBConnection3() = default;
 
-    virtual int Query() const = 0;
+  virtual int Query() const = 0;
 };
 
 class MySqlDBConnection3 : public IDBConnection3 {
-public:
-    explicit MySqlDBConnection3(std::string server_version, int protocol_version) :
-            m_info{std::move(server_version), protocol_version} {};
+ public:
+  explicit MySqlDBConnection3(std::string server_version, int protocol_version)
+      : m_info{std::move(server_version), protocol_version} {};
 
-    int Query() const override;
-    Info AdvancedQuery() const;
+  int Query() const override;
+  Info AdvancedQuery() const;
 
-private:
-    Info m_info;
+ private:
+  Info m_info;
 };
 
 class SqLiteDBConnection3 : public IDBConnection3 {
-public:
-    SqLiteDBConnection3() : m_protocol_version(0) {};
-    explicit SqLiteDBConnection3(int protocol_version) : m_protocol_version(protocol_version) {};
+ public:
+  SqLiteDBConnection3() : m_protocol_version(0){};
+  explicit SqLiteDBConnection3(int protocol_version)
+      : m_protocol_version(protocol_version){};
 
-    int Query() const override;
+  int Query() const override;
 
-private:
-    int m_protocol_version;
+ private:
+  int m_protocol_version;
 };
