722a37f81759e0730b5aca9c9cdeb6376ce88213
Konstantin Ziabrev <konstantin.ziabrev@gmail.com>
2018-07-15 08:23:26 +0300

    Fixing merge errors

--- test/ConnectionTest.cpp
+++ test/ConnectionTest.cpp
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
-=======
 /* Copyright (c) 2018 Yaroslav Stanislavyk <stl.ros@outlook.com> */
 
-#include <cassert>
->>>>>>> remotes/upstream/master
 #include <gtest/gtest.h>
 #include <cassert>
 
@@ -11,29 +7,25 @@
 
 namespace {
 
-    int GetConnectionInfo(const IDBConnection& db_connection) {
-        return db_connection.Query();
-    }
+int GetConnectionInfo(const IDBConnection& db_connection) {
+  return db_connection.Query();
+}
 
-    std::string GetConnectionAdvancedInfo(const IDBConnection& db_connection) {
-        assert(dynamic_cast<const MySqlDBConnection*>(&db_connection));
-        auto& mysql_connection = static_cast<const MySqlDBConnection&>(db_connection);
-        return mysql_connection.AdvancedQuery().server_version;
-    }
+std::string GetConnectionAdvancedInfo(const IDBConnection& db_connection) {
+  assert(dynamic_cast<const MySqlDBConnection*>(&db_connection));
+  auto& mysql_connection = static_cast<const MySqlDBConnection&>(db_connection);
+  return mysql_connection.AdvancedQuery().server_version;
+}
 
-    TEST(ConnectionInfoTest, GetInfo) {
-        MySqlDBConnection mysql_connection("5.5.8 MySQL Community Server (GPL)", 10);
-        ASSERT_EQ(10, GetConnectionInfo(mysql_connection));
-    }
-
-    TEST(ConnectionInfoTest, GetAdvancedInfo) {
-        MySqlDBConnection mysql_connection("5.5.8 MySQL Community Server (GPL)", 10);
-        ASSERT_EQ("5.5.8 MySQL Community Server (GPL)", GetConnectionAdvancedInfo(mysql_connection));
-    }
+TEST(ConnectionInfoTest, GetInfo) {
+  MySqlDBConnection mysql_connection("5.5.8 MySQL Community Server (GPL)", 10);
+  ASSERT_EQ(10, GetConnectionInfo(mysql_connection));
+}
 
 TEST(ConnectionInfoTest, GetAdvancedInfo) {
-  MySqlDBConnection connection("5.5.8 MySQL Community Server (GPL)", 10);
+  MySqlDBConnection mysql_connection("5.5.8 MySQL Community Server (GPL)", 10);
   ASSERT_EQ("5.5.8 MySQL Community Server (GPL)",
-            getConnectionAdvancedInfo(connection));
+            GetConnectionAdvancedInfo(mysql_connection));
 }
+
 }  // namespace
