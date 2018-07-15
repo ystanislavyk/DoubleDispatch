825a2de3e8c81f6167a5ce5ad5776258b5f17087
Konstantin Ziabrev <konstantin.ziabrev@gmail.com>
2018-07-15 08:03:33 +0300

    Merge conflict fix

--- src/DBConnection.cpp
+++ src/DBConnection.cpp
@@ -2,7 +2,6 @@
 
 #include "DBConnection.hpp"
 
-Info MySqlDBConnection::advancedQuery() const { return info; }
 int MySqlDBConnection::Query() const { return m_info.protocol_version; }
 
 Info MySqlDBConnection::AdvancedQuery() const { return m_info; }
