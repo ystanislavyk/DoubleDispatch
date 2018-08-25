19edd5c3d278c01695a782445a6cb7ab34a480ba
Rodion Malinovsky <rodionmalino@gmail.com>
2018-08-25 14:35:14 +0300

    Drop case3 and use case4 instead

--- src/DBConnection3.cpp
+++ src/DBConnection3.cpp
@@ -4,8 +4,16 @@
 
 IDBConnection3::~IDBConnection3() = default;
 
+ConnectionType MySqlDBConnection3::GetConnectionType() const {
+  return ConnectionType::MYSQL;
+}
+
 int MySqlDBConnection3::Query() const { return m_info.protocol_version; }
 
 Info MySqlDBConnection3::AdvancedQuery() const { return m_info; }
 
 int SqLiteDBConnection3::Query() const { return m_protocol_version; }
+
+ConnectionType SqLiteDBConnection3::GetConnectionType() const {
+  return ConnectionType::SQLITE;
+}
--- src/DBConnection3.hpp
+++ src/DBConnection3.hpp
@@ -4,9 +4,13 @@
 
 #include "DBConnection.hpp"
 
+enum class ConnectionType { MYSQL, SQLITE };
+
 class IDBConnection3 {
  public:
   virtual ~IDBConnection3();
+
+  virtual ConnectionType GetConnectionType() const = 0;
 
   virtual int Query() const = 0;
 };
@@ -15,6 +19,8 @@ class MySqlDBConnection3 : public IDBConnection3 {
  public:
   explicit MySqlDBConnection3(std::string server_version, int protocol_version)
       : m_info{std::move(server_version), protocol_version} {}
+
+  ConnectionType GetConnectionType() const override;
 
   int Query() const override;
   Info AdvancedQuery() const;
@@ -28,6 +34,8 @@ class SqLiteDBConnection3 : public IDBConnection3 {
   SqLiteDBConnection3() : m_protocol_version(0) {}
   explicit SqLiteDBConnection3(int protocol_version)
       : m_protocol_version(protocol_version) {}
+
+  ConnectionType GetConnectionType() const override;
 
   int Query() const override;
 
