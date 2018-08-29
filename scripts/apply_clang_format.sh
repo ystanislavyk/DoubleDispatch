2eb65a449eac6bde973baca9ca53e0b3e1198b4a
Morheit <Stl.ros@outlook.com>
2018-08-29 22:22:33 +0300

    Added support for macOS in the apply_clang_format script

--- scripts/apply_clang_format.sh
+++ scripts/apply_clang_format.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-find -name "*.h" -o -name "*.hpp" -o -name "*.c" -o -name "*.cpp" |
-xargs clang-format-6.0 -i
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    find -name "*.h" -o -name "*.hpp" -o -name "*.c" -o -name "*.cpp" |
+    xargs clang-format-6.0 -i
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    find . -name "*.h" -o -name "*.hpp" -o -name "*.c" -o -name "*.cpp" |
+    xargs clang-format -i
+fi
 
 echo "Done"
